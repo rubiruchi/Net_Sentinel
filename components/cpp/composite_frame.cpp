e546a1664682e36c284dc8504ca3a47db23e7e3a
EricM96 <Eric96@live.missouristate.edu>
2019-10-15 19:21:38 -0500

    packing it in for the day

--- components/cpp/composite_frame.cpp
+++ components/cpp/composite_frame.cpp
@@ -6,6 +6,18 @@
 
 using namespace std;
 
+/*
+ * Network Flow Struct
+ * 
+ * This struct contains the variables representing a single network flow 
+ * 
+ * Variables
+ * ---------
+ * saddr    string                           IP address of flow client
+ * stime    long double                      Starting time network flow
+ * ltime    long double                      Ending time of network flow
+ * tbytes   long long                        Total number of bytes sent during flow
+*/
 struct flow
 {
     string saddr;
@@ -14,6 +26,65 @@ struct flow
     long long tbytes;
 };
 
+flow *process_csv_line(string line);
+void read_csv(string fname, vector<flow *> &frame);
+void processFrame(vector<flow *> &frame);
+
+int main()
+{
+    string fnames[] = {
+        "All features/UNSW_2018_IoT_Botnet_Full5pc_1.csv",
+        "All features/UNSW_2018_IoT_Botnet_Full5pc_2.csv",
+        "All features/UNSW_2018_IoT_Botnet_Full5pc_3.csv",
+        "All features/UNSW_2018_IoT_Botnet_Full5pc_4.csv"};
+    vector<flow *> frame;
+    
+    for (string fname : fnames) 
+        read_csv(fname, frame);
+
+    processFrame(frame);
+    return 0;
+}
+
+/*
+ * Read CSV Function
+ * 
+ * This function opens the CSVs provided in the BoT IoT Dataset and extracts the
+ * information to a vector of flow structs.
+ * 
+ * Function Dependents
+ * -------------------
+ * main
+ * 
+ * Function Dependencies 
+ * ---------------------
+ * process_csv_line
+*/
+void read_csv(string fname, vector<flow *> &frame)
+{
+    string line;
+    ifstream csv(fname);
+
+    if (!csv.is_open())
+        throw "ERROR on opening file";
+
+    getline(csv, line);
+
+    while (getline(csv, line))
+    {
+        flow *newFlow = process_csv_line(line);
+        frame.push_back(newFlow);
+    }
+}
+
+/*
+ * Process CSV Line Function 
+ * 
+ * Function Dependents 
+ * -------------------
+ * read_csv
+ * main
+*/
 flow *process_csv_line(string line)
 {
     long double stime, ltime;
@@ -82,59 +153,25 @@ flow *process_csv_line(string line)
     return newFlow;
 }
 
-void read_csv(std::string fname, vector<flow *> &frame)
+/*
+ * Process Frame Function
+ * 
+ * This function takes a vector of network flows, and processes them into input
+ * for the image factory component 
+ * 
+ * Dependents
+ * ----------
+ * main
+*/
+void processFrame(vector<flow *> &frame)
 {
-    string line;
-    ifstream csv(fname);
-
-    // DELETE LATER
     int i = 0;
+    vector<string> current_frame;
 
-    if (!csv.is_open())
-        throw "ERROR on opening file";
-
-    getline(csv, line);
-
-    while (getline(csv, line))
+    for (flow *f : frame)
     {
-        flow *newFlow = process_csv_line(line);
-        frame.push_back(newFlow);
-
-        // DELETE LATER
-        i++;
-        if (i >= 5)
-            break;
+        string row;
+        /*TODO: add code for iterating that works as the vector grows*/
+        delete f;
     }
-}
-
-int main()
-{
-    int fnames_len = 4; 
-    int i; 
-    string fnames[] = {
-        "All features/UNSW_2018_IoT_Botnet_Full5pc_1.csv",
-        "All features/UNSW_2018_IoT_Botnet_Full5pc_2.csv",
-        "All features/UNSW_2018_IoT_Botnet_Full5pc_3.csv",
-        "All features/UNSW_2018_IoT_Botnet_Full5pc_4.csv"};
-
-    vector<flow *> frame;
-    for (i = 0; i < fnames_len; ++i) 
-        read_csv(fnames[i], frame);
-
-    int vec_len = frame.size();
-    for (i = 0; i < vec_len; ++i)
-    {
-        flow *aFlow = frame.front();
-        frame.erase(frame.begin());
-
-        cout << "Flow: " << i  << endl;
-        cout << "\t" << aFlow->saddr << endl;
-        cout << "\t" << aFlow->stime << endl;
-        cout << "\t" << aFlow->ltime << endl;
-        cout << "\t" << aFlow->tbytes << endl;
-
-        delete aFlow;
-    }
-
-    return 0;
 }
