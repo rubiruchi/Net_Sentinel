889f982a3dc627ec0418df04ee9047372833b4d6
Eric McCullough <eric96@live.missouristate.edu>
2019-10-20 17:08:53 -0500

    Refactor image factory to work with the data transformation module

--- components/python/image_factory.py
+++ components/python/image_factory.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-
-from composite_frame import Composite_Frame
 
 
 class Image_Factory(object):
@@ -13,8 +12,7 @@ class Image_Factory(object):
     in and populates the directory with heat map images derived from network resolutions
     in the Composite_Frame
     '''
-
-    def __init__(self, composite_frame: Composite_Frame, img_path: Path, img_limit: int = -1):
+    def __init__(self, csv_path: Path, img_path: Path):
         '''
         Instance Variables:
             @self.img_path -> path object to write heat maps too
@@ -23,36 +21,32 @@ class Image_Factory(object):
         '''
         self.img_path = img_path
         self.label = str(img_path).split('/')[-1]
+        self.img_count = 0
 
-        if img_limit < 0:
-            self.img_limit = len(composite_frame.items)
-        else:
-            self.img_limit = img_limit
+        self._create_heatmaps(csv_path)
 
-        self._create_heatmaps(composite_frame)
-
-    def _create_heatmaps(self, composite_frame: Composite_Frame):
+    def _create_heatmaps(self, csv_path: Path):
         _, ax = plt.subplots(1, 1, figsize=(1, 1), dpi=23)
-        img_id = 0
 
-        for frame in composite_frame.items:
-            # Extract necessary information and eliminate duplicate ip addresses
-            frame = frame.filter(['saddr', 'TnBPSrcIP']).groupby(
-                'saddr').sum().reset_index()
+        for file in csv_path.iterdir():
+            frame = pd.read_csv(file, usecols=['saddr', 'stime', 'ltime', 'tbytes'],
 
-            frame = pd.pivot_table(
-                frame, values='TnBPSrcIP', index='saddr', columns=None)
+                                dtype={'saddr': str, 
+                                        'stime': np.float, 
+                                        'ltime': np.float,
+                                        'tbytes': np.int
+                                        }
+                                )
+            frame = frame.filter(['saddr', 'tbytes']).groupby('saddr').sum().reset_index()
 
-            _ = sns.heatmap(frame, xticklabels=False,
-                            yticklabels=False, cbar=False)
+            frame = pd.pivot_table(frame, values='tbytes', index='saddr', columns=None)
+
+            _ = sns.heatmap(frame, xticklabels=False, yticklabels=False, cbar=False)
 
             ax.set_ylabel('')
             ax.set_xlabel('')
 
-            img_name = self.label + str(img_id)
+            img_name = self.label + str(self.img_count)
             plt.savefig(self.img_path / img_name)
 
-            img_id += 1
-
-            if img_id > self.img_limit:
-                break
+            self.img_count += 1
